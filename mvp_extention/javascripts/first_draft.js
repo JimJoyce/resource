58c2eeaf5eb7af01112a4e79fd935207af394169
Christian Barentine <cbarentine@gmail.com>
2015-05-29 16:10:33 -0500

    Changed over to ajax, need to redo messages

--- mvp_extention/javascripts/first_draft.js
+++ mvp_extention/javascripts/first_draft.js
@@ -2,40 +2,60 @@ chrome.tabs.executeScript( {
   code: "document.getSelection().toString();"
 }, function(selection) {
   console.log(selection);
-  var params = "thought="
+  var params = {};
 
   if (selection !== undefined) {
-    params += selection[0];
+    params['thought'] = selection[0];
   } else {
     console.log("Nothing found");
   }
 
 // get the url of the page
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-  params += ( '&snippet_url=' + tabs[0].url);
+chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
+  // console.log(tabs[0].url);
+  params['snippetUrl'] = tabs[0].url;
+  console.log(params);
+
+  var request = $.ajax({
+                  url: "http://localhost:3000/thoughts",
+                  method: "POST",
+                  data: params
+                });
+
 });
 
 
-var xhr = new XMLHttpRequest();
-// MAKE SURE TO CHANGE URL AND VARIABLE NAMES
-  xhr.open("POST", "http://localhost:3000/thoughts?" + params, true);
-    xhr.onreadystatechange = function(response) {
+// var xhr = new XMLHttpRequest();
+// // MAKE SURE TO CHANGE URL AND VARIABLE NAMES
+//   xhr.open("POST", "http://localhost:3000/thoughts?" + params, true);
+//     xhr.onreadystatechange = function(response) {
 
-    if (xhr.readyState == 4) {
-      console.log(xhr.responseText);
-      console.log("response complete");
-      if (xhr.status == 200) {
-          $("#saveMessage").text("Saved!");
-          console.log('Saved!');
-          window.setTimeout(window.close, 1000);
-      } else {
-          // Show what went wrong
-          $("#saveMessage").text('Error saving: ' + xhr.statusText);
-          console.log( 'Error saving: ' + xhr.statusText);
-      }
-    }
-  };
-  xhr.send();
+//     if (xhr.readyState == 4) {
+//       // console.log(xhr.responseText);
+//       console.log("response complete");
+//       if (xhr.status == 200) {
+//           $("#saveMessage").text("Saved!");
+//           console.log('Saved!');
+//           // window.setTimeout(window.close, 1000);
+//       } else {
+//           // Show what went wrong
+//           $("#saveMessage").text('Error saving: ' + xhr.statusText);
+//           console.log( 'Error saving: ' + xhr.statusText);
+//       }
+//     }
+//   };
+//   xhr.send();
+
+// var request = $.ajax({
+//                   url: "http://localhost:3000/thoughts",
+//                   method: "POST",
+//                   data: params
+//                 });
+
+// request.done(function (response) {
+//   console.log("Recieved Response!");
+//   console.log(response);
+// });
 
 });
 

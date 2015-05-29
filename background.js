e86450329101254c77f237ea63f9b8035d4eab07
JimJoyce <j.joyce92@gmail.com>
2015-05-29 16:23:42 -0500

    slides out and stuff

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 console.log( 'Background.html starting!' );
 	/*Send request to current tab when page action is clicked*/
+
+
 	chrome.browserAction.onClicked.addListener(function(tab) {
 		chrome.tabs.getSelected(null, function(tab) {
 			chrome.tabs.sendRequest(
--- responderScript.js
+++ responderScript.js
@@ -21,22 +21,64 @@ function toggleSidebar() {
 	else {
 		var sidebar = document.createElement('div');
 		sidebar.id = "mySidebar";
-		sidebar.innerHTML = "\
-			<h1>Hello</h1>\
-			World!\
-		";
-		sidebar.style.cssText = "\
-			position:fixed;\
-			top:0px;\
-			right:0px;\
-			width:25%;\
-			height:100%;\
-			background:white;\
-			box-shadow:inset 0 0 1em black;\
-			z-index:999999;\
-		";
-		document.body.appendChild(sidebar);
+		sidebar.innerHTML = "<h1>Drop what you found here</h1>";
+		document.firstElementChild.appendChild(sidebar);
+		styleBar("#" + sidebar.id);
 		sidebarOpen = true;
 	}
 };
+
+function styleBar (id) {
+	var bodyText = $(id).children();
+	$(id).toggle();
+	$(id).css({
+		"position": "absolute",
+		"right": "0",
+		"top": "5%",
+		"width": "15%",
+		"height": "90%",
+		"background-color": "gray",
+		"border-bottom-left-radius": "2em",
+		"border-top-left-radius": "2em",
+		"box-shadow": "0px 6px 10px 2px rgba(0, 0, 0, .3)"
+	});
+
+	$(bodyText).css({
+		"font-family":"Open-Sans",
+		"text-align": "center",
+		"color": "#EAEAEA",
+		"margin-top": "75%",
+		"font-weight": "100"
+	});
+    var options = { direction: 'left'};
+    $(id).slideToggle("fast");
+};
+
+// .id{
+//   animation: slideIn ease-in-out 1s;
+//   animation-iteration-count: 1;
+//   transform-origin: 50% 50%;
+//   animation-fill-mode:forwards; /*when the spec is finished*/
+//   -webkit-animation: slideIn ease-in-out 1s;
+//   -webkit-animation-iteration-count: 1;
+//   -webkit-transform-origin: 50% 50%;
+//   -webkit-animation-fill-mode:forwards; /*Chrome 16+, Safari 4+*/
+// }
+
+// @keyframes slideIn{0% {transform:translate(-1px,0px);}52% {transform:translate(-194px,1px);}70% {transform:translate(-178px,0px);}100%{transform:translate(-183px,1px);}}
+
+// @-webkit-keyframes slideIn {
+//   0% {
+//     -webkit-transform:  translate(-1px,0px)  ;
+//   }
+//   52% {
+//     -webkit-transform:  translate(-194px,1px)  ;
+//   }
+//   70% {
+//     -webkit-transform:  translate(-178px,0px)  ;
+//   }
+//   100% {
+//     -webkit-transform:  translate(-183px,1px)  ;
+//   }
+// }
 

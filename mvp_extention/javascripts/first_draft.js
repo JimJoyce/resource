1e83e147df8d622df010750ef78b1b54f6f8d0ca
Christian Barentine <cbarentine@gmail.com>
2015-05-29 13:44:47 -0500

    Added first draft of mvp extension

---
+++ mvp_extention/javascripts/first_draft.js
@@ -0,0 +1,29 @@
+chrome.tabs.executeScript( {
+  code: "document.getSelection().toString();"
+}, function(selection) {
+  console.log(selection[0]);
+  var params = "thought=" + selection[0];
+
+var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:3000/thoughts?" + params, true);
+    xhr.onreadystatechange = function(response) {
+
+    if (xhr.readyState == 4) {
+      var test = JSON.parse(xhr.response);
+      console.log(xhr.responseText);
+      console.log("response complete");
+      if (xhr.status == 200) {
+
+          console.log('Saved!');
+          // window.setTimeout(window.close, 1000);
+      } else {
+          // Show what went wrong
+          console.log( 'Error saving: ' + xhr.statusText);
+      }
+    }
+  };
+  xhr.send();
+
+});
+
+
